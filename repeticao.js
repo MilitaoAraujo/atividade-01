ca89c00ab7ec963c9e942a8023cb32d8e6413232
MilitaoAraujo <militao.araujo@aluno.uepb.edu.br>
2025-08-16 10:25:07 -0300

    analisar a quantidade de letras em uma palavra

---
+++ repeticao.js
@@ -0,0 +1,6 @@
+const nome = "javascript";// Definindo a variável nome com o valor "javascript"
+let controle = 0;// Inicializando a variável controle com 0
+for (let i = 0; i < nome.length; i++) {// Loop para percorrer cada letra da variável nome
+    controle++;//adiciona 1 ao controle a cada iteração
+}
+console.log("a palavra tem " + controle + " letras");// Imprime o número total de letras na variável nome
