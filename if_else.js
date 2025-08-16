605bfedd8555f428a4001c6c91b06737cbb3d596
MilitaoAraujo <militao.araujo@aluno.uepb.edu.br>
2025-08-16 10:19:57 -0300

    estrutura de controle para comparação de valores

---
+++ if_else.js
@@ -0,0 +1,11 @@
+num1 = 10;// cria a variável num1 e atribui o valor 10
+num2 = 20;// cria a variável num2 e atribui o valor 20
+if (num1 > num2) {// verifica se num1 é maior que num2
+    console.log("o primeiro é maior");// imprime mensagem se for verdadeiro
+}
+else if (num1 < num2) {// verifica se num1 é menor que num2
+    console.log("o segundo é maior");// imprime mensagem se for verdadeiro
+}   
+else {// se não for maior nem menor, então são iguais
+    console.log("são iguais");// imprime mensagem se for verdadeiro
+}
