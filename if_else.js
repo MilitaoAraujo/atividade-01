8c00df429997a1033b5870bae8205d072c1c13ef
MilitaoAraujo <militao.araujo@aluno.uepb.edu.br>
2025-08-16 10:20:46 -0300

    mudnaça nos valores comparados na funcão

--- if_else.js
+++ if_else.js
@@ -1,5 +1,5 @@
-num1 = 10;// cria a variável num1 e atribui o valor 10
-num2 = 20;// cria a variável num2 e atribui o valor 20
+num1 = 50;// cria a variável num1 e atribui o valor 10
+num2 = 30;// cria a variável num2 e atribui o valor 20
 if (num1 > num2) {// verifica se num1 é maior que num2
     console.log("o primeiro é maior");// imprime mensagem se for verdadeiro
 }
